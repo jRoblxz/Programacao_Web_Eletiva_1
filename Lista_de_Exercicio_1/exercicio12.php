cea84031820d142da9a7aea365d36998df0db941
jRoblxz <joaoroblez76@gmail.com>
2025-08-25 21:53:42 -0300

    Implementando exercicio 12 e atualizando o 11 + atualizando o css

--- Lista_de_Exercicio_1/exercicio12.php
+++ Lista_de_Exercicio_1/exercicio12.php
@@ -4,19 +4,23 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Exercício 11</title>
+    <title>Exercício 12</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 
 <body>
     <main class="container">
-        <h1>Exercício 11</h1>
+        <h1>Exercício 12</h1>
         <form method="post">
             <div class="row inline-row mb-3">
-                <div class="col-md-12">
-                    <label for="valor1" class="form-label">Insira o raio de um círculo:</label>
+                <div class="col-md-6">
+                    <label for="valor1" class="form-label">Insira o valor da base:</label>
                     <input type="number" id="valor1" name="valor1" class="form-control" required="">
+                </div>
+                <div class="col-md-6">
+                    <label for="valor2" class="form-label">Insira o valor do expoente:</label>
+                    <input type="number" id="valor2" name="valor2" class="form-control" required="">
                 </div>
             </div> 
             <div class="d-flex justify-content-between mt-3">
@@ -34,9 +38,10 @@
         </form>
         <?php
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $valor1 = $_POST["valor1"]; // leitura da variavel
-            $perimetro = 2 * $valor1 * pi();
-            echo "<div class='result_div'><p class='resultado'>O perímetro do círculo é: $perimetro</p></div>";
+            $valor1 = $_POST["valor1"];
+            $valor2 = $_POST["valor2"];
+            $potencia = $valor1 ** $valor2;
+            echo "<div class='result_div'><p class='resultado'>$valor1 elevado a $valor2 é: $potencia</p></div>";
         }
         ?>
     </main>
