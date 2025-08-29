97208413a5cc375bc5eb94cd5332f3dc34c4022b
jRoblxz <joaoroblez76@gmail.com>
2025-08-28 21:26:38 -0300

    alterando telas 11 e 12

--- Lista_de_Exercicio_1/exercicio11.php
+++ Lista_de_Exercicio_1/exercicio11.php
@@ -10,7 +10,134 @@
 </head>
 
 <body>
-    <main class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Tarefas</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Exemplos Bootstrap
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown1">
+                            <li><a class="dropdown-item" href="#">Exemplo 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 5</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 1
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown2">
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio1.php">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio2.php">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio3.php">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio4.php">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio5.php">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio6.php">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio7.php">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio8.php">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio9.php">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio10.php">Exercício 10</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio11.php">Exercício 11</a></li>
+                            <li><a class="dropdown-item" href="./Lista_de_Exercicio_1/exercicio12.php">Exercício 12</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 13</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 14</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 15</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 16</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 17</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 18</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 19</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 20</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 2
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown3">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 10</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 11</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 12</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 13</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 14</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 15</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 16</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 17</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 18</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 19</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 20</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown4" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 3
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown4">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 10</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown5" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 4
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown5">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 7</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown6" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 4
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown6">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container_1">
         <h1>Exercício 11</h1>
         <form method="post">
             <div class="row inline-row mb-3">
@@ -25,8 +152,8 @@
 
                 <!-- Botões Voltar e Próximo à direita -->
                 <div class="d-flex gap-2">
-                    <button class="btn btn-primary" type="button" onclick="window.location.href='exercicio11.php'">Voltar</button>
-                    <button class="btn btn-primary" type="button" onclick="window.location.href='exercicio13.php'">Próximo</button>
+                    <button class="btn btn-primary" type="button" onclick="window.location.href='exercicio10.php'">Voltar</button>
+                    <button class="btn btn-primary" type="button" onclick="window.location.href='exercicio12.php'">Próximo</button>
                 </div>
             </div>
 
--- Lista_de_Exercicio_1/exercicio12.php
+++ Lista_de_Exercicio_1/exercicio12.php
@@ -10,6 +10,136 @@
 </head>
 
 <body>
+    <!-- Cabeçalho principal início -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Tarefas</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Exemplos Bootstrap
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown1">
+                            <li><a class="dropdown-item" href="#">Exemplo 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exemplo 5</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 1
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown2">
+                            <li><a class="dropdown-item" href="./exercicio1.php">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="./exercicio2.php">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="./exercicio3.php">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="./exercicio4.php">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="./exercicio5.php">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="./exercicio6.php">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="./exercicio7.php">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="./exercicio8.php">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="./exercicio9.php">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="./exercicio10.php">Exercício 10</a></li>
+                            <li><a class="dropdown-item" href="./exercicio11.php">Exercício 11</a></li>
+                            <li><a class="dropdown-item" href="./exercicio12.php">Exercício 12</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 13</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 14</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 15</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 16</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 17</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 18</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 19</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 20</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 2
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown3">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 10</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 11</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 12</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 13</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 14</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 15</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 16</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 17</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 18</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 19</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 20</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown4" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 3
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown4">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 10</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown5" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 4
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown5">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 7</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown6" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista 4
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown6">
+                            <li><a class="dropdown-item" href="#">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="#">Exercício 5</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Cabeçalho principal fim -->
+
     <main class="container">
         <h1>Exercício 12</h1>
         <form method="post">
@@ -24,17 +154,12 @@
                 </div>
             </div> 
             <div class="d-flex justify-content-between mt-3">
-                <!-- Botão Enviar à esquerda -->
                 <button type="submit" class="btn btn-success">Enviar</button>
-
-                <!-- Botões Voltar e Próximo à direita -->
                 <div class="d-flex gap-2">
                     <button class="btn btn-primary" type="button" onclick="window.location.href='exercicio11.php'">Voltar</button>
                     <button class="btn btn-primary" type="button" onclick="window.location.href='exercicio13.php'">Próximo</button>
                 </div>
             </div>
-
-
         </form>
         <?php
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
